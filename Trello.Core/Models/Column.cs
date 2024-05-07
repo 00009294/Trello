3b85b8ea84c57b333653fcb42e10ca0e9584bdc4
fayzu <fayzu@HP>
2024-05-07 17:48:46 +0500

    Add project files.

---
+++ Trello.Core/Models/Column.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trello.Core.Models
+{
+    public class Column : BaseEntity
+    {
+        public Board? Board { get; set; }
+        public int BoardId { get; set; }
+    }
+}
